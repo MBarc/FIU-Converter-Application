62b183886c612b8c228ca70798287ad55ab27599
MBarc <42979055+MBarc@users.noreply.github.com>
2019-10-31 11:53:14 -0400

    created information.py

---
+++ Server/information.py
@@ -0,0 +1,22 @@
+'''
+This script will hold information for other scripts that depend on it
+'''
+
+import os
+import collections
+
+from converter import complete_path
+
+extensions = collections.defaultdict(int)
+
+for path, dirs, files in os.walk(complete_path):
+   for filename in files:
+       extensions[os.path.splitext(filename)[1].lower()] += 1
+
+print(extensions)
+
+extensions = []
+extensions_count = []
+for key,value in extensions.items():
+    extensions.append(key)
+    extensions_count.append(value)
