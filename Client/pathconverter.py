2f6e0a79724cc76345e5e23608a91def1de0886a
MBarc <42979055+MBarc@users.noreply.github.com>
2019-10-31 10:15:30 -0400

    Considers upper and lowercase letters simultaneously
    -Changed alphabet to contain only uppercase letters
    -Added "file_path.startswith(letter.lower() + ":")" to check for lowercase letters at the same time
    -Added some comments

--- Client/pathconverter.py
+++ Client/pathconverter.py
@@ -1,16 +1,16 @@
+'''
+This script with convert the user's input from Windows format to Linux format. 
+This is the prep the information for the Linux server.
+'''
 from information import file_path
 
-alphabet = ['A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i', \
-            'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r', \
-            'S', 's', 'T', 't', 'U', 'u', 'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z']
+alphabet = ['A', 'B', 'C', 'D', 'E', 'F','G', 'H','I', \
+            'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', \
+            'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
 for letter in alphabet:
-
-    if file_path.startswith(letter + ':'):
-
-        file_path = file_path[2:]
-
-        count = file_path.count('\\')
-        
-        file_path = file_path.replace('\\','/', count)
-
+            #If file path starts with a letter (either uppercase or lowercase). . . 
+            if file_path.startswith(letter + ':') or file_path.startswith(letter.lower() + ":"):
+                        file_path = file_path[2:]
+                        count = file_path.count('\\')
+                        file_path = file_path.replace('\\','/', count) # Ready for Linux server
