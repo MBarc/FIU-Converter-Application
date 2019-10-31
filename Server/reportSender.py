935e61699e55f1060ff4a9020e7b2f1ca0ee003d
MBarc <42979055+MBarc@users.noreply.github.com>
2019-10-31 12:49:24 -0400

    Update reportSender.py

--- Server/reportSender.py
+++ Server/reportSender.py
@@ -1,3 +1,13 @@
 '''
 This script will send the report back to the user's Windows machine
 '''
+
+
+'''
+Do research on: sshpass
+
+Relevant URLs:
+https://askubuntu.com/questions/191390/how-to-use-sudo-command-to-install-tar-gz/191391
+https://stackoverflow.com/questions/50096/how-to-pass-password-to-scp
+
+'''
