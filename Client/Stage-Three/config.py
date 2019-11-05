80f63a3b245649aa0ac6811621f9d90d99a5aeb6
MBarc <42979055+MBarc@users.noreply.github.com>
2019-11-05 12:52:50 -0500

    Delete config.py

--- Client/Stage-Three/config.py
+++
@@ -1,9 +0,0 @@
-'''
-This file contains specific settings for the Haivision Converter Application.
-'''
-
-portal = dict(
- url = "www.ExampleURL.com",
- username = "username",
- password = "password",
-)
