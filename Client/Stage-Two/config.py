4abf9e9c75ed00984b660c628059695f27da7996
MBarc <42979055+MBarc@users.noreply.github.com>
2019-11-05 12:51:05 -0500

    added comments

--- Client/Stage-Two/config.py
+++ Client/Stage-Two/config.py
@@ -3,12 +3,12 @@ This file contains specific settings for the Haivision Converter Application.
 '''
 
 server = dict(
- ip = "xxx.xxx.xxx.xxx",
- port = 25,
+ ip = "xxx.xxx.xxx.xxx", # IP adress for Linux machine
+ port = 25, # SSH port for Linux machine
 )
 
 portal = dict(
- url = "www.ExampleURL.com",
+ url = "www.ExampleURL.com", # URL for HMP portal
  username = "username",
  password = "password",
 )
