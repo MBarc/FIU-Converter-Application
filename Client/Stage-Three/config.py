17353b709f1063a9abd83b258837bcea9cdf6748
MBarc <42979055+MBarc@users.noreply.github.com>
2019-11-05 09:35:43 -0500

    Update config.py

--- Client/Stage-Three/config.py
+++ Client/Stage-Three/config.py
@@ -1,1 +1,9 @@
+'''
+This file contains specific settings for the Haivision Converter Application.
+'''
 
+portal = dict(
+ url = "www.ExampleURL.com",
+ username = "username",
+ password = "password",
+)
