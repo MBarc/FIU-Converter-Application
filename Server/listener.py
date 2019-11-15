b9cefe3dbd9410a667716cf06df3d114def3c0c1
MBarc <42979055+MBarc@users.noreply.github.com>
2019-11-15 10:58:09 -0500

    Converted ideal execution from daemon to crontab.
    This script will now be execute as a cron job in order to take up less system resources.

---
+++ Server/listener.py
@@ -0,0 +1,87 @@
+'''
+This is a daemon that will detect when new information gets shared with the Linux server.
+Runs once an hour.
+
+Note: Assumes a new info.txt will appear within every 7 years.
+'''
+import os
+import subprocess
+import time
+from daemon import runner
+import psutil #to check PID names
+import os, time, datetime, calendar
+from datetime import date
+
+def logger(message):
+	#Logs error messages
+	error_long = open("Error.txt", "a")
+	error_log.write(message)
+	error_log.close()
+
+
+info = os.getcwd() + "/info.txt"
+
+try:
+	if os.path.exists(info):
+						
+	'''Creation information for info.txt'''
+	creation_time = time.ctime(os.path.getctime(info)) #parse data from this
+	day = creation_time[:-21]
+	hour = int(creation_time[11:-11])
+	month = str(creation_time[4:7])
+						
+	'''Time information when this script is executed'''
+	current_day = calendar.day_name[date.today().weekday()] #parse data from this
+	current_day_initial = current_day[:-6]
+	current_hour = datetime.datetime.now().hour
+
+	month_number = datetime.datetime.now().month #returns the int of the month
+
+	#determining what month it is by the given number
+	if month_number == 1:
+		current_month = "Jan"
+	elif month_number == 2:
+		current_month = "Feb"
+	elif month_number == 3:
+		current_month = "Mar"
+	elif month_number == 4:
+		current_month = "Apr"
+	elif month_number == 5:	
+		current_month = "May"
+	elif month_number == 6:
+		current_month = "Jun"
+	elif month_number == 7:
+		current_month = "Jul"
+	elif month_number == 8:
+		current_month = "Aug"
+	elif month_number == 9:
+		current_month == "Sep"
+	elif month_number == 10:
+		current_month = "Oct"
+	elif month_number == 11:
+		current_month = "Nov"
+	elif month_number == 12:
+		current_month = "Dec"
+						
+	#If info.txt is less than 1 hour old, then start converter.py
+	if hour == current_hour and day == current_day_initial and month == current_month:
+							
+		pids = [pid for pid in os.listdir('/proc') if pid.isdigit()] #gets a list of all pids in /proc
+
+		tracker = [] #keeps track of pids with ffmpeg
+		for pid in pids:
+			process_name = psutil.Process(pid) # returns the name of the pid
+
+			if "ffmpeg" in process_name:
+				tracker.append(process_name)
+
+		if len(tracker = 0): #if there's not a process with ffmpeg as a substring ; if tracker is empty
+			os.system("python3 /haivisionconverterapp/converter.py") #command to start converter.py
+		else:
+			pass
+	else:
+		pass
+except Exception as e:
+	logger("[Lisenter ERROR]:", str(e))
+
+		
